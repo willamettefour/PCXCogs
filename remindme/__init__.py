b210dd4e86f582e1d22f2b0336dd32d8b5770623
willamette <94886283+willamettefour@users.noreply.github.com>
2024-08-12 19:55:22 -0700

    refresh

--- remindme/__init__.py
+++ remindme/__init__.py
@@ -1,15 +1,13 @@
 """Package for RemindMe cog."""
-
 import json
-from pathlib import Path
 
+from pathlib import Path
 from redbot.core.bot import Red
 
 from .remindme import RemindMe
 
-with Path(__file__).parent.joinpath("info.json").open() as fp:
+with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
 
 async def setup(bot: Red) -> None:
     """Load RemindMe cog."""
